ca890ee43f7adaa7045418b2ab0fa659df5a0e56
Ks121099 <kunalsinha_bt2k17@dtu.ac.in>
2020-04-02 01:01:55 +0530

    Display menu made

---
+++ main.py
@@ -0,0 +1,41 @@
+import time
+import curses
+stdcsr=curses.initscr()
+
+menu=['Home', 'Play','Score', 'Exit']
+def mai_menu(stdcsr, select_id):
+    stdcsr.clear()
+    h,w=stdcsr.getmaxyx()
+    for i,row in enumerate(menu):
+        x=w//2-len(row)//2
+        y=h//2- len(menu)//2+ i
+        if i==select_id:
+            stdcsr.attron(curses.color_pair(1))
+            stdcsr.addstr(y,x,row)
+            stdcsr.attroff(curses.color_pair(1))
+        else:
+            stdcsr.addstr(y,x,row)
+    stdcsr.refresh()
+
+
+def main(stdcsr):
+    curses.curs_set(0)  
+    curr_row_id=0
+    curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_WHITE)
+    mai_menu(stdcsr, curr_row_id)
+    while 1:
+        ip=stdcsr.getch()
+        stdcsr.clear()
+        if ip==curses.KEY_DOWN:
+           curr_row_id=(curr_row_id+1)%len(menu)  
+        elif ip==curses.KEY_UP:
+            if curr_row_id==0:
+                curr_row_id=len(menu)-1
+            else:
+                curr_row_id=curr_row_id-1  
+
+        mai_menu(stdcsr, curr_row_id)
+        stdcsr.refresh()
+    curses.curs_set(1)  
+
+curses.wrapper(main)
